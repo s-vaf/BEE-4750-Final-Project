7234970c86e818ae3af774ed0d4d892cb2084e08
s-vaf <sv439@cornell.edu>
2025-12-15 22:41:31 -0500

    idk

--- scripts/setup_packages.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-project_root = normpath(joinpath(@__DIR__, ".."))
-Pkg.activate(project_root)
-Pkg.add(["JuMP", "HiGHS"]) 
-Pkg.instantiate()
-Pkg.precompile()
-println("Package setup complete.")
